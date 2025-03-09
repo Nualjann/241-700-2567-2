b6cda3fa9e58513325aedefc75f4a8ba1f31fa18
Nualjann <nualjan.k@ku.th>
2025-03-09 12:51:00 +0700

    Update

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function submitData(){
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstName === '') {
+        errors.push('กรุณากรอกชื่อ');
+    }
+    if (!userData.lastName === '') {
+        errors.push('กรุณากรอกนามสกุล');
+    }
+    if (!userData.age === '') {
+        errors.push('กรุณากรอกอายุ');
+    }
+    if (!userData.gender === '') {
+        errors.push('กรุณาเลือกเพศ');
+    }
+    if (!userData.interests === '') {
+        errors.push('กรุณาเลือกสิ่งที่สนใจ');
+    }
+    if (!userData.description === '') {
+        errors.push('กรุณากรอกคำอธิบาย');
+   return errors;
+}
+}
+
+const submitData = async () => {
     let firstnameDOM = document.querySelector('input[name=firstname]');
     let lastnameDOM = document.querySelector('input[name=lastname]');
     let ageDOM = document.querySelector('input[name=age]');
     let genderDOM = document.querySelector('input[name=gender]');
     let interestDOM = document.querySelectorAll('input[name=interest]:checked');
     let descriptionDOM = document.querySelector('textarea[name=description]');
-    
+
+    let messageDOM = document.getElementById('message');
+
+try {
     let interest = ''
     for(let i = 0; i < interestDOM.length; i++){
         interest += interestDOM[i].value 
@@ -20,7 +46,43 @@ function submitData(){
         age: ageDOM.value,
         gender: genderDOM.value,
         description: descriptionDOM.value,
-        interest: interest
+        interests: interest
     }
-    console.log('submitData',userData);
+    console.log('submitData', userData);
+   /* const errors = validateData(userData)
+    if(errors.length > 0){
+       //มี error
+       throw {
+              message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+              errors: errors
+       } 
+    }
+       */
+
+    const response = await axios.post('http://localhost:8000/users', userData)
+    console.log('response', response.data);
+    messageDOM.innerText = 'บันทึกข้อมูลเรียบร้อย'
+    messageDOM.className = 'massage success'
+}catch(error){
+    console.log('error.message', error.message);
+    console.log('error', error.errors);
+    
+    if (error.response) {
+    console.log(error.response)
+    error.message = error.response.data.message
+    error.errors = error.response.data.errors
+    }
+
+    let htmlData = '<div>'
+    htmlData += `<div>${error.message}</div>`
+    htmlData += '<ul>'
+    for (let i = 0; i < error.errors.length; i++){
+        htmlData += `<li>${error.errors[i]}</li>`
+    }
+    htmlData += '</ul>'
+    htmlData += '</div>'
+
+    messageDOM.innerText = 'บันทึกข้อมูลไม่สำเร็จ'
+    messageDOM.className = 'massage danger'
+}
 }
